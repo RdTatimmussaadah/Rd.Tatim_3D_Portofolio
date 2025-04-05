584c857dc77a4856acc2ce210cb0d9f60f260184
RdTatimmussaadah <timitati22@gmail.com>
2025-04-05 19:52:46 +0700

    third commit

--- src/section/Projects.jsx
+++ src/section/Projects.jsx
@@ -48,7 +48,7 @@ const Projects = ({}) =>{
         <section className="c-space my-20" id="work">
             <p className="head-text">Projects</p>
 
-            <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
+            <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full h-full">
                 <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
                     <div className="absolute top-0 right-0">
                         <img src={currentProject.spotlight} alt="spotlight" className="w-full h-96 object-cover rounded-xl"/>
@@ -65,34 +65,36 @@ const Projects = ({}) =>{
                         <p className="animatedText">{currentProject.subdesc}</p>
                     </div>
 
-                    <div className="flex items-center justify-between flex-wrap gap-5">
-                        <div className="flex items-center gap-3">
-                        {currentProject.tags.map((tag, index) => (
-                            <div key={index} className="tech-logo">
-                            <img src={tag.path} alt={tag.name} />
+                    <div className="mt-auto">
+                        <div className="flex items-center justify-between flex-wrap gap-5">
+                            <div className="flex items-center gap-3">
+                            {currentProject.tags.map((tag, index) => (
+                                <div key={index} className="tech-logo">
+                                <img src={tag.path} alt={tag.name} />
+                                </div>
+                            ))}
                             </div>
-                        ))}
                         </div>
-                    </div>
 
-                    {/* <a
-                        className="flex items-center gap-2 cursor-pointer text-white-600"
-                        href={currentProject.href}
-                        target="_blank"
-                        rel="noreferrer">
-                            <p>Check Live Site</p>
-                            <img src="/assets/arrow-up.png" alt="arrow" className="w-3 h-3" />
-                    </a> */}
+                        {/* <a
+                            className="flex items-center gap-2 cursor-pointer text-white-600"
+                            href={currentProject.href}
+                            target="_blank"
+                            rel="noreferrer">
+                                <p>Check Live Site</p>
+                                <img src="/assets/arrow-up.png" alt="arrow" className="w-3 h-3" />
+                                </a> */}
 
 
-                    <div className="flex justify-between items-center mt-7">
-                        <button className="arrow-btn" onClick={() => handleNavigation('previous')}>
-                        <img src="/assets/left-arrow.png" alt="left arrow" />
-                        </button>
+                        <div className="flex justify-between items-center mt-7">
+                            <button className="arrow-btn" onClick={() => handleNavigation('previous')}>
+                            <img src="/assets/left-arrow.png" alt="left arrow" />
+                            </button>
 
-                        <button className="arrow-btn" onClick={() => handleNavigation('next')}>
-                        <img src="/assets/right-arrow.png" alt="right arrow" className="w-4 h-4" />
-                        </button>
+                            <button className="arrow-btn" onClick={() => handleNavigation('next')}>
+                            <img src="/assets/right-arrow.png" alt="right arrow" className="w-4 h-4" />
+                            </button>
+                        </div>
                     </div>
                 </div>
 
